90043e48851112ddd9504cefba307a6074e36150
Mondal10 <amitmondal111.am@gmail.com>
2020-05-25 21:13:24 +0530

    structuring products route with GET, POST, PATCH and DELETE request

---
+++ api/routes/products.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+
+// Handle GET requests on /products route
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handle GET request Here are your products at /products'
+    });
+});
+
+// Handle POST requests on /products route
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handle POST request Here are your products at /products'
+    });
+});
+
+// GET a product with ID
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    if (id === 'shoes') {
+        res.status(200).json({
+            message: 'Get product with ID shoes',
+            id
+        });
+    } else {
+        res.status(200).json({
+            message: 'whattttt is this id'
+        });
+    }
+});
+
+// PATCH or update a product
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    res.status(200).json({
+        message: 'updated the product'
+    });
+});
+
+// DELETE or update a product
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    res.status(200).json({
+        message: 'deleted the product'
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works'
-    });
-});
+const productRoutes = require('./api/routes/products');
+
+app.use('/products', productRoutes);
 
 module.exports = app;
