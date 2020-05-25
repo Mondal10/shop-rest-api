2edb88bcf42d39242fb5f61eae0ec3cfb5407f04
Mondal10 <amitmondal111.am@gmail.com>
2020-05-25 22:27:35 +0530

    added nodemon and handling errors

--- app.js
+++ app.js
@@ -7,4 +7,21 @@ const orderRoutes = require('./api/routes/orders');
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+// Handle 404 error
+app.use('/', (req, res, next) => {
+    const error = new Error('Not Found');
+
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    const { status, message } = error;
+    res.status(status || 500);
+
+    res.json({
+        message
+    });
+});
+
 module.exports = app;
