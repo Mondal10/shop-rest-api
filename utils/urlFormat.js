40b403d0fc2d435c6129acca2f44fdf694a98638
Mondal10 <amitmondal111.am@gmail.com>
2020-05-30 12:28:00 +0530

    comment added

--- utils/urlFormat.js
+++ utils/urlFormat.js
@@ -1,5 +1,10 @@
 const url = require('url');
 
+/**
+ * Get the full URL from the request and return it
+ * 
+ * @param {Request} req 
+ */
 const getFullURL = (req) => url.format({
     protocol: req.protocol,
     host: req.get('host'),
