83ce65bf5893d5da628e8db44a22773908309e77
Mondal10 <amitmondal111.am@gmail.com>
2020-05-30 19:20:14 +0530

    adding controller for user and migrating the functionalities

---
+++ api/controllers/user.js
@@ -0,0 +1,126 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+const getAllUsers = (req, res, next) => {
+    User.find()
+        .select('_id email') // select only needed fields(columns)
+        .exec()
+        .then(user => {
+            res.status(200).json(user);
+        });
+};
+
+const userSignup = (req, res, next) => {
+    const { email, password } = req.body;
+
+    User.find({ email })
+        .exec()
+        .then(user => {
+            if (user.length) {
+                return res.status(409).json({
+                    message: 'User with same email already exists'
+                })
+            }
+
+            bcrypt.hash(password, 10, (err, hash) => {
+                if (err) {
+                    return res.status(500).json({
+                        error: err
+                    });
+                }
+
+                const user = new User({
+                    _id: new mongoose.Types.ObjectId(),
+                    email,
+                    password: hash
+                });
+
+                user.save()
+                    .then(result => {
+                        res.status(201).json(result);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({ error: err });
+                    });
+            });
+        })
+};
+
+const userLogin = (req, res, next) => {
+    const { email, password } = req.body;
+
+    User.findOne({ email })
+        .exec()
+        .then(user => {
+            if (!user) {
+                // Better to send 401 then 404 to avoid filtering(emails) in bruteforce attack
+                return res.status(401).json({
+                    message: 'Authentication Failed'
+                });
+            }
+
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (err) {
+                    // Better to send 401 then 404 to avoid filtering(emails) in bruteforce attack
+                    return res.status(401).json({
+                        message: 'Authentication Failed'
+                    });
+                }
+
+                // result will always be a boolean as per documentation of bcrypt
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            userId: user._id,
+                            email: user.email
+                        },
+                        process.env.JWT_SECRET,
+                        {
+                            expiresIn: "1h"
+                        }
+                    );
+
+                    return res.status(200).json({
+                        message: 'Authenticated Successfully',
+                        token
+                    });
+                }
+
+                // When password is incorrect
+                return res.status(401).json({
+                    message: 'Authentication Failed'
+                });
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+};
+
+const deleteUser = (req, res, next) => {
+    const id = req.params.userId;
+
+    User.deleteOne({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'User Deleted successfully',
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+};
+
+module.exports = {
+    getAllUsers,
+    userSignup,
+    userLogin,
+    deleteUser
+};
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,124 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-const User = require('../models/user');
 const authentication = require('../middleware/authentication');
+const { getAllUsers, userSignup, userLogin, deleteUser } = require('../controllers/user');
 
-router.get('/all', authentication, (req, res, next) => {
-    User.find()
-        .select('_id email') // select only needed fields(columns)
-        .exec()
-        .then(user => {
-            res.status(200).json(user);
-        });
-})
+router.get('/all', authentication, getAllUsers);
 
-router.post('/signup', (req, res, next) => {
-    const { email, password } = req.body;
+router.post('/signup', userSignup);
 
-    User.find({ email })
-        .exec()
-        .then(user => {
-            if (user.length) {
-                return res.status(409).json({
-                    message: 'User with same email already exists'
-                })
-            }
+router.post('/login', userLogin);
 
-            bcrypt.hash(password, 10, (err, hash) => {
-                if (err) {
-                    return res.status(500).json({
-                        error: err
-                    });
-                }
-
-                const user = new User({
-                    _id: new mongoose.Types.ObjectId(),
-                    email,
-                    password: hash
-                });
-
-                user.save()
-                    .then(result => {
-                        res.status(201).json(result);
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        res.status(500).json({ error: err });
-                    });
-            });
-        })
-});
-
-router.post('/login', (req, res, next) => {
-    const { email, password } = req.body;
-
-    User.findOne({ email })
-        .exec()
-        .then(user => {
-            if (!user) {
-                // Better to send 401 then 404 to avoid filtering(emails) in bruteforce attack
-                return res.status(401).json({
-                    message: 'Authentication Failed'
-                });
-            }
-
-            bcrypt.compare(password, user.password, (err, result) => {
-                if (err) {
-                    // Better to send 401 then 404 to avoid filtering(emails) in bruteforce attack
-                    return res.status(401).json({
-                        message: 'Authentication Failed'
-                    });
-                }
-
-                // result will always be a boolean as per documentation of bcrypt
-                if (result) {
-                    const token = jwt.sign(
-                        {
-                            userId: user._id,
-                            email: user.email
-                        },
-                        process.env.JWT_SECRET,
-                        {
-                            expiresIn: "1h"
-                        }
-                    );
-
-                    return res.status(200).json({
-                        message: 'Authenticated Successfully',
-                        token
-                    });
-                }
-
-                // When password is incorrect
-                return res.status(401).json({
-                    message: 'Authentication Failed'
-                });
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ error: err });
-        });
-});
-
-router.delete('/:userId', authentication, (req, res, next) => {
-    const id = req.params.userId;
-
-    User.deleteOne({ _id: id })
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'User Deleted successfully',
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ error: err });
-        });
-});
+router.delete('/:userId', authentication, deleteUser);
 
 module.exports = router;
