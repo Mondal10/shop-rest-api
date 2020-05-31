b0e390ab62e8a6078824bc3a73493db2d37a068a
Mondal10 <amitmondal111.am@gmail.com>
2020-05-31 13:40:42 +0530

    mino update

--- api/middleware/authentication.js
+++ api/middleware/authentication.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : '';
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
